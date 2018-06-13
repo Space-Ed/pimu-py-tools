b156d2702c257884edab95bb02aacae6b32b4e06
Edward Dalley <endalley11@gmail.com>
2018-06-13 22:47:43 +0000

    add midi proxy

--- midi-proxy.py
+++ midi-proxy.py
@@ -1,15 +1,45 @@
-#! /bin/python3
+#! python3
+import mido
+import sys
+import signal
 
-def create_source:
-    pass
+def create_source(midi_dev_name):
+    return  mido.open_input(midi_dev_name)
 
-def create_sink:
-    pass
+def create_sink(midi_dev_name):
+    return mido.open_output(midi_dev_name)
+
+def forwardMessage(sink):
+    def forwarder(msg):
+
+        if (msg.type != 'clock'):
+            sink.send(msg)
+            print(msg)
+
+
+    return forwarder
 
 def connect(source, sink):
-    pass
+    source.callback = forwardMessage(sink)
 
+if __name__=='__main__':
+    inputPortName = sys.argv[1]
+    outputPortName  = sys.argv[2]
 
-if __name__=='main':
-    connect(create_source(), create_sink())
+    try:
+        source = create_source(inputPortName)
+        sink = create_sink(outputPortName)
+    except OSError as err:
+        print(err)
+        sys.exit(1)
+
+    print("setting up link from '%s' to '%s'" % (inputPortName, outputPortName))
+    connect(source, sink)
+
+    def signal_handler(signal, frame):
+        print("closing ports and exiting")
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
 
