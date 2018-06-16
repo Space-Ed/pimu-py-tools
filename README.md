656846c39bbd9ecad32be292310061073a163c7c
edward <endalley11@gmail.com>
2018-06-17 10:39:55 +1200

    refactor to keystate map

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pimu-py-tools
 Leveraging mod-host, midi, lv2, jack for musical synthesis on the pi
+
+
+# Installation
+
+install python3
+install mido, rtmidi
--- midi-sustainer.py
+++ midi-sustainer.py
@@ -9,10 +9,24 @@ def create_source(midi_dev_name):
 def create_sink(midi_dev_name):
     return mido.open_output(midi_dev_name)
 
+def key_channel(message):
+    return (message.channel, message.note)
+
+class Tape:
+    def __init__(self):
+        pass
+
+class KeyState:
+    def __init__(self, message):
+        self.note = message.note,
+        self.channel = message.channel,
+        self.held = False,
+        self.on = False,
+        self.tape = Tape()
+
 class Sustainer:
     def __init__(self, output, sustainCC):
-        self.notesPlaying = set()
-        self.notesHeld = set()
+        self.keys = dict()
         self.output = output
         self.sustain = False
         self.sustainCC = sustainCC
@@ -26,7 +40,7 @@ class Sustainer:
                 else:
                     self.sustainOff()
             elif message.control == 3:
-                if message.value == 127: 
+                if message.value == 127:
                     self.holdSustain()
                 else:
                     self.unholdSustain()
@@ -37,26 +51,41 @@ class Sustainer:
         else:
             self.output.send(message)
 
+    def getKeyState(self, message):
+        key = key_channel(message)
+        state = self.keys.get(key)
+
+        if (state is None):
+            state = KeyState
+
+        return state
+
+    def heldNotes(self, is_held=True):
+        return filter(lambda ks: ks.held == is_held, self.keys.values())
+    def onNotes(self, is_on=False):
+        return filter(lambda ks: ks.on == is_on, self.keys.values())
+
     def noteOn(self, message):
-        note = message.note
-        self.notesHeld.add(note)
+        keystate = self.getKeyState(message)
+        keystate.held = True
 
         if self.sustain:
-            if note in self.notesPlaying:
-                self.output.send(mido.Message('note_off', note=note))
-
-            self.notesPlaying.add(note)
+            if keystate.on:
+                self.output.send(mido.Message('note_off', note=keystate.note, keystate.channel))
             self.output.send(message)
         else:
             self.output.send(message)
 
+        keystate.on = True
+
     def noteOff(self, message):
-        note = message.note
-        self.notesHeld.remove(note)
+        keystate = self.getKeyState(message)
+        keystate.held = False
 
         #note off only turns off when sustain is off
         if not self.sustain:
             self.output.send(message)
+            keystate.on = False
 
     def holdSustain(self):
         self.sustainHeld = True
@@ -70,9 +99,6 @@ class Sustainer:
             self.sustainOff()
 
     def sustainOn(self):
-        if not self.sustainHeld:
-            self.notesPlaying = self.notesHeld.copy()
-
         self.sustain = True;
 
     def sustainOff(self):
@@ -82,9 +108,10 @@ class Sustainer:
         self.sustain = False
 
         # send note off for unheld notes
-        for note in self.notesPlaying:
-            if not note in self.notesHeld:
-                self.output.send(mido.Message('note_off', note=note))
+        for ks in self.onNotes():
+            if not ks.held:
+                self.output.send(mido.Message('note_off', note=ks.note, channel=ks.channel))
+                ks.on = False
 
     def panic():
         pass
@@ -116,4 +143,3 @@ if __name__=='__main__':
 
     signal.signal(signal.SIGINT, signal_handler)
     signal.pause()
-
